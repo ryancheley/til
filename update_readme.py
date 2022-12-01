767cbf123c2e34aaa87130499b7703bd695034c0
Ryan Cheley <rcheley@gmail.com>
2022-11-30 19:00:17 -0800

    :tractor: More refactoring

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,4 @@
-"Run this after build_database.py - it needs tils.db"
+"Run this after build_database.py - it needs til.db"
 import pathlib
 import sqlite_utils
 import sys
@@ -7,12 +7,9 @@ import re
 root = pathlib.Path(__file__).parent.resolve()
 
 index_re = re.compile(r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL)
-count_re = re.compile(r"<!\-\- count starts \-\->.*<!\-\- count ends \-\->", re.DOTALL)
-
-COUNT_TEMPLATE = "<!-- count starts -->{}<!-- count ends -->"
 
 if __name__ == "__main__":
-    db = sqlite_utils.Database(root / "tils.db")
+    db = sqlite_utils.Database(root / "til.db")
     by_topic = {}
     for row in db["til"].rows_where(order_by="created_utc"):
         by_topic.setdefault(row["topic"], []).append(row)
@@ -33,8 +30,6 @@ if __name__ == "__main__":
         readme = root / "README.md"
         index_txt = "\n".join(index).strip()
         readme_contents = readme.open().read()
-        rewritten = index_re.sub(index_txt, readme_contents)
-        rewritten = count_re.sub(COUNT_TEMPLATE.format(db["til"].count), rewritten)
-        readme.open("w").write(rewritten)
+        readme.open("w").write(index_re.sub(index_txt, readme_contents))
     else:
         print("\n".join(index))
